47c1c65b340e667645de08dae6b47ab08967e605
Magalí Díaz <yajaira281096@gmail.com>
2023-07-09 23:17:03 -0300

    modifique proyecto

--- js/index.js
+++ js/index.js
@@ -1,42 +1,168 @@
-const tableBody = document.querySelector('tbody')
-const inputSearch = document.querySelector('input#inputSearch')
+const tableBody = document.querySelector('tbody');
+const inputSearch = document.querySelector('input#inputSearch');
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
 
-const armarFilaHTML = (prod)=> {
-    return `<tr>
-                <td class="class-table-number">${prod.id}</td>
-                <td class="imgProductos"><img src="${prod.imagen}"</td>
-                <td>${prod.nombre}</td>
-                <td>$ ${prod.precio}</td>
-                <td><button id="${prod.id}" class="button button-outline button-big-emoji">❤️</button></td>
-            </tr>`
-}
+const armarFilaHTML = (prendas) => {
+  return `<tr>
+              <td class="class-table-number">${prendas.id}</td>
+              <td class="imgProductos"><img src="${prendas.imagen}"></td>
+              <td>${prendas.nombre}</td>
+              <td>$ ${prendas.precio}</td>
+              <td><button id="${prendas.id}" class="button button-outline">comprar</button></td>
+          </tr>`;
+};
 
-const filtrarProductos = ()=> {
-    let arrayResultado = prendas.filter((producto)=> producto.nombre.toLowerCase().includes(inputSearch.value.trim().toLowerCase()))
-    if (arrayResultado.length > 0) {
-        cargarProductos(arrayResultado)
+const filtrarProductos = () => {
+  let arrayResultado = prendas.filter((prenda) => prenda.nombre.toLowerCase().includes(inputSearch.value.trim().toLowerCase()));
+  if (arrayResultado.length > 0) {
+      cargarProductos(arrayResultado);
+  }
+};
+inputSearch.addEventListener('search', filtrarProductos);
+
+const cargarProductos = (array) => {
+  tableBody.innerHTML = '';
+  array.forEach((prendas) => {
+      tableBody.innerHTML += armarFilaHTML(prendas);
+  });
+  activarClickEnBotonesComprar();
+};
+
+const activarClickEnBotonesComprar = () => {
+  const botonesComprar = document.querySelectorAll("button.button.button-outline");
+  for (let botonComprar of botonesComprar) {
+      botonComprar.addEventListener('click', () => {
+          let resultadoProducto = prendas.find((prenda) => prenda.id === parseInt(botonComprar.id));
+          if (resultadoProducto && resultadoProducto.nombre) {
+              Carrito.push(resultadoProducto);
+              guardarEnLocalStorage();
+              mensajeToast(resultadoProducto.nombre)
+          }
+      });
+  }
+};
+
+const finalizarCompra = () => {
+  if (Carrito.length > 0) {
+    Carrito = [];
+    guardarEnLocalStorage();
+
+    const orderNumber = generateOrderNumber();
+
+    modalContainer.style.display = "none";
+
+    alert(orderNumber);
+  }
+};
+
+const generateOrderNumber = () => {
+  const min = 100000;
+  const max = 999999;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+  modalContainer.style.display = "none";
+
+  const pintarCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">Carrito</h1>
+    `;
+    modalContainer.append(modalHeader);
+  
+    const modalButton = document.createElement("h1");
+    modalButton.innerText = "X";
+    modalButton.className = "modal-header-button";
+  
+    modalButton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+  
+    modalHeader.append(modalButton);
+  
+    if (Carrito.length > 0) {
+      Carrito.forEach((prendas) => {
+        let CarritoContent = document.createElement("div");
+        CarritoContent.className = "modal-content";
+        CarritoContent.innerHTML = `
+            <img src="${prendas.imagen}">
+            <h3>${prendas.nombre}</h3>
+            <p>$ ${prendas.precio}</p>
+        `;
+        modalContainer.append(CarritoContent);
+  
+        let eliminar = document.createElement("span");
+  
+        eliminar.innerText = "❌";
+        eliminar.className = "eliminar-prenda";
+        CarritoContent.append(eliminar);
+  
+        eliminar.addEventListener("click", eliminarProducto);
+      });
+  
+      const total = Carrito.reduce((acc, prenda) => acc + prenda.precio, 0);
+      const totalBullyng = document.createElement("div");
+      totalBullyng.className = "total-content";
+      totalBullyng.innerHTML = `Total a pagar: $ ${total}`;
+      modalContainer.append(totalBullyng);
+  
+      const finalizarCompraButton = document.createElement("button");
+      finalizarCompraButton.innerText = "Finalizar compra";
+      finalizarCompraButton.className = "button button-primary";
+      finalizarCompraButton.addEventListener("click", finalizarCompra);
+      modalContainer.append(finalizarCompraButton);
+    } else {
+      const emptyCartMessage = document.createElement("p");
+      emptyCartMessage.innerText = "No hay productos en el carrito";
+      modalContainer.append(emptyCartMessage);
     }
-}
-inputSearch.addEventListener('search', filtrarProductos)
+  };
+  
+  verCarrito.addEventListener("click", pintarCarrito);
 
-const cargarProductos = (array)=> {
-    tableBody.innerHTML = ''
-    array.forEach((producto) => {
-        tableBody.innerHTML += armarFilaHTML(producto)
-    })
-    activarClickEnBotonesFav()
-}
+const eliminarProducto = (event) => {
+  let eliminar = event.target;
+  let CarritoContent = eliminar.parentNode;
+  let producto = CarritoContent.querySelector("h3").innerText;
 
-const activarClickEnBotonesFav = ()=> {
-    const botonesFav = document.querySelectorAll('button.button.button-outline.button-big-emoji')
-    for (let botonFav of botonesFav) {
-        botonFav.addEventListener('click', ()=> {
-            let resultadoProducto = prendas.find((prod)=> prod.id === parseInt(botonFav.id))
-            favoritos.push(resultadoProducto)
-            guardarEnLocalStorage()
-            mostrarMensajes(`El producto ${resultadoProducto.nombre} se guardó en favoritos...`, 'green')
-        })
+  Carrito = Carrito.filter((prenda) => prenda.nombre !== producto);
+  CarritoContent.remove();
+
+  const total = Carrito.reduce((acc, prenda) => acc + prenda.precio, 0);
+  const totalBullyng = document.querySelector(".total-content");
+  totalBullyng.innerHTML = `Total a pagar: ${total} $`;
+};
+
+function mensajeToast(prendas) {
+  Toastify ({
+    text: `${prendas} se agregó al carrito`,
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "darkorange",
+      color: "white",
     }
+  }).showToast ();
 }
 
-cargarProductos(prendas)
+function alert(orderNumber) {
+  Swal.fire({
+    title: 'Gracias por tu compra',
+    text: `Tu N° de orden es: ${orderNumber} envía este N° al cel: 099101010 para continuar`,
+    icon: 'success',
+    confirmButtonText: 'OK',
+    customClass: {
+      confirmButton: 'my-custom-confirm-button-class',
+      popup: 'my-custom-popup-class'
+    },
+  })
+}
+cargarProductos(prendas);
--- js/productos.js
+++ js/productos.js
@@ -1,40 +1,44 @@
+const carrito = []
+
 const prendas = [
-    { id: 1, imagen:"Imagenes/Clasicas/azul.jpg", nombre: "Hoodie Clásica Azul", precio: 2600, categoria: "Clásicas" },
-    { id: 2, imagen: "Imagenes/Clasicas/beige.jpg", nombre: "Hoodie Clásica Beige", precio: 2600, categoria: "Clásicas" },
-    { id: 3, imagen: "Imagenes/Clasicas/blanca.jpg", nombre: "Hoodie Clásica Blanca", precio: 2600, categoria: "Clásicas" },
-    { id: 4, imagen: "Imagenes/Clasicas/celeste.jpg", nombre: "Hoodie Clásica Celeste", precio: 2600, categoria: "Clásicas" },
-    { id: 5, imagen: "Imagenes/Clasicas/gris.jpg", nombre: "Hoodie Clásica Gris", precio: 2600, categoria: "Clásicas" },
-    { id: 6, imagen: "Imagenes/Clasicas/negra.jpg", nombre: "Hoodie Clásica Negra", precio: 2600, categoria: "Clásicas" },
-    { id: 7, imagen: "Imagenes/Clasicas/roja.jpg", nombre: "Hoodie Clásica Roja", precio: 2600, categoria: "Clásicas" },
-    { id: 8, imagen: "Imagenes/Clasicas/rosa.jpg", nombre: "Hoodie Clásica Rosa", precio: 2600, categoria: "Clásicas" },
-    { id: 9, imagen: "Imagenes/Clasicas/verde.jpg", nombre: "Hoodie Clásica Verde", precio: 2600, categoria: "Clásicas" },
-    { id: 10, imagen: "Imagenes/con Diseño/brake head.jpg", nombre: "Hoodie con Diseño Brake Head", precio: 3200, categoria: "con Diseño" },
-    { id: 11, imagen: "Imagenes/con Diseño/emoji 🫠.jpg", nombre: "Hoodie con Diseño Emoji 🫠", precio: 3200, categoria: "con Diseño" },
-    { id: 12, imagen: "Imagenes/con Diseño/flamenco.jpg", nombre: "Hoodie con Diseño Flamenco", precio: 3200, categoria: "con Diseño" },
-    { id: 13, imagen: "Imagenes/con Diseño/humo.jpg", nombre: "Hoodie con Diseño Humo", precio: 3200, categoria: "con Diseño" },
-    { id: 14, imagen: "Imagenes/con Diseño/pink head.jpg", nombre: "Hoodie con Diseño Pink Head", precio: 3200, categoria: "con Diseño" },
-    { id: 15, imagen: "Imagenes/con Diseño/ted.jpg", nombre: "Hoodie con Diseño Ted", precio: 3200, categoria: "con Diseño" },
-    { id: 16, imagen: "Imagenes/con Diseño/tedy panda.jpg", nombre: "Hoodie con Diseño Tedy Panda", precio: 3200, categoria: "con Diseño" },
+    { id: 1, imagen:"Imagenes/Clasicas/azul.jpg", nombre: "Hoodie Clasica Azul", precio: 2600, categoria: "Clásicas"},
+    { id: 2, imagen: "Imagenes/Clasicas/beige.jpg", nombre: "Hoodie Clasica Beige", precio: 2600, categoria: "Clásicas"},
+    { id: 3, imagen: "Imagenes/Clasicas/blanca.jpg", nombre: "Hoodie Clasica Blanca", precio: 2600, categoria: "Clásicas"},
+    { id: 4, imagen: "Imagenes/Clasicas/celeste.jpg", nombre: "Hoodie Clasica Celeste", precio: 2600, categoria: "Clásicas"},
+    { id: 5, imagen: "Imagenes/Clasicas/gris.jpg", nombre: "Hoodie Clasica Gris", precio: 2600, categoria: "Clásicas"},
+    { id: 6, imagen: "Imagenes/Clasicas/negra.jpg", nombre: "Hoodie Clasica Negra", precio: 2600, categoria: "Clásicas"},
+    { id: 7, imagen: "Imagenes/Clasicas/roja.jpg", nombre: "Hoodie Clasica Roja", precio: 2600, categoria: "Clásicas"},
+    { id: 8, imagen: "Imagenes/Clasicas/rosa.jpg", nombre: "Hoodie Clasica Rosa", precio: 2600, categoria: "Clásicas"},
+    { id: 9, imagen: "Imagenes/Clasicas/verde.jpg", nombre: "Hoodie Clasica Verde", precio: 2600, categoria: "Clásicas"},
+    { id: 10, imagen: "Imagenes/con Diseño/brake head.jpg", nombre: "Hoodie con Diseño Brake Head", precio: 3200, categoria: "con Diseño"},
+    { id: 11, imagen: "Imagenes/con Diseño/emoji 🫠.jpg", nombre: "Hoodie con Diseño Emoji 🫠", precio: 3200, categoria: "con Diseño"},
+    { id: 12, imagen: "Imagenes/con Diseño/flamenco.jpg", nombre: "Hoodie con Diseño Flamenco", precio: 3200, categoria: "con Diseño"},
+    { id: 13, imagen: "Imagenes/con Diseño/humo.jpg", nombre: "Hoodie con Diseño Humo", precio: 3200, categoria: "con Diseño"},
+    { id: 14, imagen: "Imagenes/con Diseño/pink head.jpg", nombre: "Hoodie con Diseño Pink Head", precio: 3200, categoria: "con Diseño"},
+    { id: 15, imagen: "Imagenes/con Diseño/ted.jpg", nombre: "Hoodie con Diseño Ted", precio: 3200, categoria: "con Diseño"},
+    { id: 16, imagen: "Imagenes/con Diseño/tedy panda.jpg", nombre: "Hoodie con Diseño Tedy Panda", precio: 3200, categoria: "con Diseño"},
 ]
 
-const guardarEnLocalStorage = ()=> {
-    if (favoritos.length > 0) {
-        localStorage.setItem('Favoritos', JSON.stringify(favoritos))
-    }
-}
+const guardarEnLocalStorage = () => {
+  if (carrito.length > 0) {
+      localStorage.setItem("Carrito", JSON.stringify(carrito))
+  } else {
+      localStorage.removeItem("Carrito");
+  }
+};
 
-const recuperarFavsDeLocalStorage = ()=> {
-    if (localStorage.getItem('Favoritos')) {
-        return JSON.parse(localStorage.getItem('Favoritos'))
-    } else {
-        return []
-    }
-}
+const recuperarCarritoDeLocalStorage = () => {
+  if (localStorage.getItem("Carrito")) {
+      return JSON.parse(localStorage.getItem("Carrito"));
+  } else {
+      return [];
+  }
+};
 
-const favoritos = recuperarFavsDeLocalStorage()
+const mostrarMensajes = (msg, bgcolor) => {
+  const divMsg = document.querySelector("div.toast-msg");
+  divMsg.textContent = msg || "";
+  divMsg.style.background = bgcolor || "darkslateblue;";
+};
 
-const mostrarMensajes = (msg, bgcolor)=> {
-    const divMsg = document.querySelector('div.toast-msg')
-    divMsg.textContent = msg || ''
-    divMsg.style.background = bgcolor || 'darkslateblue;'
-}
+let Carrito = recuperarCarritoDeLocalStorage()
